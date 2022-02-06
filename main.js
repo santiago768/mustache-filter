fa6a0550642c031ce6f00c7000559e6accb8e5cf
santiago768 <83921416+santiago768@users.noreply.github.com>
2022-02-06 10:08:00 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 nosex="";
 nosey="";
+  
 
+function preload(){
+    imagenose=loadImage("https://i.postimg.cc/6QS4LnhH/clownnose.png")
+}
 
 function setup() {
     canvas = createCanvas(300, 300);
@@ -12,9 +16,11 @@ function setup() {
     model.on('pose',getresults);
 }
 function draw(){
-    image(video,0,0,300,300)
-    fill("red");
-    circle(nosex,nosey+15,15);
+    image(video,0,0,300,300);
+    // fill("red");
+    // circle(nosex,nosey,15);
+    image(imagenose,nosex-10,nosey-10,20,20);
+
     
 }
 
